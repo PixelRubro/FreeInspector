03f6f3ef71a42f440e7276a5a4d531ccdf135ce3
Angelo <matheussavioti@gmail.com>
2021-01-12 00:23:23 -0300

    updating validator base class

--- Editor/Drawers/AbstractDrawers/PropertyValidationDrawer.cs
+++ Editor/Drawers/AbstractDrawers/PropertyValidationDrawer.cs
@@ -6,23 +6,5 @@ namespace YoukaiFox.Inspector
     public abstract class PropertyValidationDrawer: YoukaiAttributeDrawer, IWarningMessage
     {
         public abstract string GetWarningMessage();
-
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
-        {
-            EditorGUI.BeginChangeCheck();
-            EditorGUI.PropertyField(position, property, label);
-
-            if (EditorGUI.EndChangeCheck())
-            {
-                var value = property.objectReferenceValue;
-
-                if (value == null || IsObjectValid(value, property))
-                    return;
-
-                property.objectReferenceValue = null;
-            }
-        }
-        
-        protected abstract bool IsObjectValid(Object value, SerializedProperty property);
     }
 }
--- Editor/Drawers/PrefabAttributeDrawer.cs
+++ Editor/Drawers/PrefabAttributeDrawer.cs
@@ -11,9 +11,27 @@ namespace YoukaiFox.Inspector
             return "Not a prefab instance!";
         }
 
-        protected override bool IsObjectValid(Object value, SerializedProperty property)
+        private bool IsPropertyValid(Object value, SerializedProperty property)
         {
             return PrefabUtility.GetPrefabAssetType(value) != PrefabAssetType.NotAPrefab;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
+        {
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(position, property, label);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                var value = property.objectReferenceValue;
+
+                if ((value == null) || (IsPropertyValid(value, property)))
+                {
+                    return;
+                }
+
+                property.objectReferenceValue = null;
+            }
         }
     }
 }
