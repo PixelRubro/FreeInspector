4ac087b8ebd77040690d1aa28dd069338b2e96fb
Angelo <matheussavioti@gmail.com>
2021-01-13 02:24:49 -0300

    adding conditional help box base class

---
+++ Attributes/AbstractAttributes/ConditionalHelpAttribute.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using UnityEditor;
+
+namespace YoukaiFox.Inspector
+{
+    /// <summary>
+    /// Base class for comparison attributes that show a help box.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
+    public abstract class ConditionalHelpAttribute : PropertyAttribute
+    {
+        public string HelpText { get; set; }
+        public MessageType MessageType { get; set; }
+        public string TargetConditionName { get; set; }
+
+        /// <summary>
+        /// Handles help box display based on the condition.
+        /// </summary>
+        public ConditionalHelpAttribute(string targetConditionName, string helpText, MessageType messageType = MessageType.Info)
+        {
+            TargetConditionName = targetConditionName;
+            HelpText = helpText;
+            MessageType = messageType;
+        }
+    }
+}
