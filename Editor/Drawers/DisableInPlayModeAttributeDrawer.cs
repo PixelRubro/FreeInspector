7b401c3b09fd88eb72d704a5078c757968399032
MSavioti <angelosavioti@gmail.com>
2021-03-31 13:44:30 -0300

    added disable in play mode attribute

---
+++ Editor/Drawers/DisableInPlayModeAttributeDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace YoukaiFox.Inspector
+{
+    [CustomPropertyDrawer(typeof(DisableInPlayModeAttribute))]
+    public class DisableInPlayModeAttributeDrawer : YoukaiPropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (Application.isPlaying) 
+                GUI.enabled = false;
+                
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+}
