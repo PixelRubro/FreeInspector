97931421a8232afc093155a9763a9307b740bbee
MSavioti <angelosavioti@gmail.com>
2021-03-31 03:32:49 -0300

    added password attribute

---
+++ Editor/Drawers/PasswordAttributeDrawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace YoukaiFox.Inspector
+{
+    [CustomPropertyDrawer(typeof(PasswordAttribute))]
+    public class PasswordAttributeDrawer : YoukaiPropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            label = EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, label);
+            property.stringValue = EditorGUI.PasswordField(position, GUIContent.none, property.stringValue);
+            EditorGUI.EndProperty();
+        }
+    }
+}
